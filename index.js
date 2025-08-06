ebe6a7db0076bb03d2ff54794db0190f81cc896c
itkhurramshahzad0408-debug <it.khurramshahzad0408@gmail.com>
2025-08-06 14:42:19 +0500

    done

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ app.get('/alldata', (req, res) => {
   console.log('GET:', req.query);
   res.send('Data received');
 });
-const CONSUMER_KEY = '43c88df41d1a18257be9701095b445ac63c78da946f4bbfbc4464b5ef39219fa';
-const CONSUMER_SECRET = 'b6537c32de9eb3e9da3c496160d3e98e6a892a3608663e5fc7ecc57354328d2e';
-const TOKEN_ID = '10b4f67ebee0b8c9e66114413283a9c70b971f4710da2b34b8f4728cbb5ea65f';
-const TOKEN_SECRET = '2c915518a4b55f96c1bdfe4bf0fe665599196590a784ffc137dd023fd4ffb906';
-const ACCOUNT_ID = '7952908';
-// comment 
+const CONSUMER_KEY = '4a85783fbc881bb4ba17cdb55b40cc928478cc20a40ff8d9d679030ea705dbf5';
+const CONSUMER_SECRET = 'be3b8a61692fce1273388427338dc138f6f0cf2b529a01e0924ae8927bad6b01';
+const TOKEN_ID = 'd70097b4ef495ab04860691990139d0a32223b2fdd2315bc9c8d82d0c446aa71';
+const TOKEN_SECRET = '9412c4121bf8238565630c04f698eb36eac97cb0d7ce95b5cc6ed1ad1686fa6e';
+const ACCOUNT_ID = 'TD3025443';
 const oauth = OAuth({
   consumer: { key: CONSUMER_KEY, secret: CONSUMER_SECRET },
   signature_method: 'HMAC-SHA256',
@@ -28,7 +27,7 @@ const oauth = OAuth({
 app.post('/shopify/order', async (req, res) => {
   var orderData = req.body;
   var request_data = {
-    url: `https://${ACCOUNT_ID}.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=323&deploy=1`,
+    url: `https://${ACCOUNT_ID}.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=10322&deploy=1`,
     method: 'POST',
   };
 
@@ -59,3 +58,7 @@ app.post('/shopify/order', async (req, res) => {
 app.listen(3000, () => {
   console.log('Middleware running on port 3000');
 });
+
+
+
+module.exports = app;
