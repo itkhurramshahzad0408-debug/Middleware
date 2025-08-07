7b30d4326378c85433c94946e4710db10e46e5ba
itkhurramshahzad0408-debug <it.khurramshahzad0408@gmail.com>
2025-08-07 10:37:55 +0500

    orderdata update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const oauth = OAuth({
 
 app.post('/shopify/order', async (req, res) => {
   var orderData = req.body;
-  console.log(orderData);
+  console.log("orderData" , orderData);
   orderData = orderData.first_name;
   var request_data = {
     url: `https://td3025443.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=10323&deploy=1`,
