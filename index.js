66e429ee327707f03f88f5b8c255304f47dec1eb
itkhurramshahzad0408-debug <it.khurramshahzad0408@gmail.com>
2025-09-02 11:48:11 +0500

    testing

--- index.js
+++ index.js
@@ -109,8 +109,9 @@ app.post('/api/testLink1', async (req, res) => {
     // Step 3: Prepare 1LINK IBFT / Raast API payload
     const oneLinkPayload = req.body;
 
+    console.log('khurram', 'khurram');
     console.log('link payload', oneLinkPayload);
-    console.log('khurram')
+    console.log('khurram', 'khurram');
     // Step 4: Call 1LINK IBFT API
     const ibftResponse = await axios.post('https://sandboxapi.1link.net.pk/uat-1link/sandbox/1Link/statusInquiry', oneLinkPayload, {
       headers: {
